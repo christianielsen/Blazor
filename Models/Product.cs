9384ece9568bc509c54e262103d383775c2acc4f
Chris <chris.nielsen1226@gmail.com>
2025-03-28 22:57:00 +0000

    Model binding

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+namespace LearnBlazor.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsActive { get; set; }
+    public double Price { get; set; }
+}
