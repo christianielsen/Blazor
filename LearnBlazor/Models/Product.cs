f0f8c04741b1b83eeef92a0b05a1767374b2f9f5
Chris <chris.nielsen1226@gmail.com>
2025-04-03 22:53:36 +0100

    Add extra properties to product

--- LearnBlazor/Models/Product.cs
+++ LearnBlazor/Models/Product.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LearnBlazor.Models;
 
 public class Product
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    [Required] public string Name { get; set; }
     public bool IsActive { get; set; }
-    public double Price { get; set; }
+    [Range(1, 1000)] public double Price { get; set; }
     public IEnumerable<ProductProperty> Properties { get; set; }
+    public Category Category { get; set; }
+    public DateOnly AvailableAfter { get; set; }
+}
+
+public enum Category
+{
+    Entree,
+    Appetizer,
+    Dessert
 }
