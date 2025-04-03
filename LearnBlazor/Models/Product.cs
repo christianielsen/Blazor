2b29e662517ffd6b142a28fb0ae0fea60f4a8827
Chris <chris.nielsen1226@gmail.com>
2025-04-03 23:15:42 +0100

    Default available after date to today

--- LearnBlazor/Models/Product.cs
+++ LearnBlazor/Models/Product.cs
@@ -10,7 +10,7 @@ public class Product
     [Range(1, 1000)] public double Price { get; set; }
     public IEnumerable<ProductProperty> Properties { get; set; }
     public Category Category { get; set; }
-    public DateOnly AvailableAfter { get; set; }
+    public DateOnly AvailableAfter { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 }
 
 public enum Category
