75422771f05b815880dafaf3ad510f6c6f2826b3
Chris <chris.nielsen1226@gmail.com>
2025-04-06 22:40:21 +0100

    Add category page

--- LearnBlazorProject/Program.cs
+++ LearnBlazorProject/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using LearnBlazorProject.Components;
 using LearnBlazorProject.Components.Account;
 using LearnBlazorProject.Data;
+using LearnBlazorProject.Repository;
+using LearnBlazorProject.Repository.IRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
